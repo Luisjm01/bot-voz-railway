b16305dc54e725a774bcb8ae5c1a97ad9d0aecd4
Luisjm01 <ljmagallon@gmail.com>
2025-04-24 13:15:48 -0700

    Add files via upload

---
+++ public/public/scripts.js
@@ -0,0 +1,4 @@
+
+// Aquí va el código JS original para manejo de grabación y reproducción del audio
+console.log("Toscanito UI cargado");
+// Este archivo debe incluir las funciones de grabar, detener, reproducir, y actualizar el DOM
